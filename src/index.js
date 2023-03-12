1f7edec7e12e3ec55433ab772d558dfaed8b50d4
Abed <abedj2023@Gmail.com>
2023-03-11 21:14:43 -0600

    setup react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello Abed");
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <h1>My Stranger's Page</h1>;
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
